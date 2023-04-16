f7755945bbaeb2d0935b3a47440c57ecc06c4b3f
ebubekirtatlilioglu <tatlilioglu_ebubekir@hotmail.com>
2023-04-16 17:09:35 +0300

    Add project files.

---
+++ KeepNoteApi/Program.cs
@@ -0,0 +1,38 @@
+using KeepNoteApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var cs = builder.Configuration.GetConnectionString("BaglantiCumlem");
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(cs));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options=>options.AddDefaultPolicy(policy=>policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));////////////////////////
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();////////////////////////////
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
